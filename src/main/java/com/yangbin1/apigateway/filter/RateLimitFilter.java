d2964f0360178046f786a9e51996346d20aef93b
yangbin1 <yangbin199191@163.com>
2019-05-20 22:43:42 +0800

    333

--- src/main/java/com/yangbin1/apigateway/filter/RateLimitFilter.java
+++ src/main/java/com/yangbin1/apigateway/filter/RateLimitFilter.java
@@ -1,14 +1,19 @@
 package com.yangbin1.apigateway.filter;
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
 public class RateLimitFilter extends ZuulFilter {
+
+    //每秒产生1000令牌
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(1000);
 
     @Override
     public String filterType() {
@@ -34,6 +39,11 @@ public class RateLimitFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        if(!RATE_LIMITER.tryAcquire()){
+            requestContext.setSendZuulResponse(false);
+            requestContext.setResponseStatusCode(HttpStatus.TOO_MANY_REQUESTS.value());
+        }
         return null;
     }
 }
